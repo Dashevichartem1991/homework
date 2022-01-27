19c469d6aa8f1ce3d00525b4123a48620c7d5757
Dashevichartem1991 <dashevichartem@gmail.com>
2022-01-27 22:23:07 +0300

    add exerciseNumber1

---
+++ src/exerciseNumber1.java
@@ -0,0 +1,17 @@
+public class exerciseNumber1 {
+    public static void main(String[] args) {
+        int [] array = new int[7];
+        int someNum = 15;
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 7;
+            if (array[i] == someNum){
+                continue;
+            }else{
+                sum += array[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
